a1edf1ae27b7ab03e66dd5e4b159d125a22437dc
Matthew Kosloski <matthewkosloski@me.com>
2019-12-15 22:31:50 -0600

    added yaml config and implemented wp cli

--- Vagrantfile
+++ Vagrantfile
@@ -1,6 +1,10 @@
 # -*- mode: ruby -*-
 # vi: set ft=ruby :
 
+require 'yaml'
+
+settings = YAML.load_file 'settings.yml'
+
 # All Vagrant configuration is done below. The "2" in Vagrant.configure
 # configures the configuration version (we support older styles for
 # backwards compatibility). Please don't change it unless you know what
@@ -10,7 +14,19 @@ Vagrant.configure("2") do |config|
     # Ubuntu 18.04 LTS
     config.vm.box = "ubuntu/bionic64"
 
-    config.vm.provision :shell, path: "bootstrap.sh"
+    config.vm.provision "shell" do |s|
+        s.path = "bootstrap.sh"
+        s.args = [
+            settings['site']['ip'],
+            settings['site']['mysql_database'],
+            settings['site']['mysql_username'],
+            settings['site']['mysql_password'],
+            settings['site']['wordpress_username'],
+            settings['site']['wordpress_password'],
+            settings['site']['wordpress_email'],
+            settings['site']['wordpress_title']
+        ]
+    end
 
     # Allows access to port 80 on the guest using port 8080 on 
     # the host.
@@ -19,7 +35,7 @@ Vagrant.configure("2") do |config|
     # Allows access to the guest from the host via static IP. The
     # IP chosen should be in the reserved private address space, 
     # which is 192.168.0.0 – 192.168.255.255.
-    config.vm.network "private_network", ip: "192.168.50.4"
+    config.vm.network "private_network", ip: settings['site']['ip']
 
     # Link the /var/www/html folder on the guest to the ./www/
     # folder on the host.
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,13 @@
+IP_ADDRESS=$1
+MYSQL_DATABASE=$2
+MYSQL_USERNAME=$3
+MYSQL_PASSWORD=$4
+WORDPRESS_USERNAME=$5
+WORDPRESS_PASSWORD=$6
+WORDPRESS_EMAIL=$7
+WORDPRESS_TITLE=$8
+URL="http://$IP_ADDRESS/"
+
 # Update dpkg package database and upgrade packages.
 apt-get update && apt-get upgrade
 
@@ -16,13 +26,31 @@ apt-get install -y php7.3 php7.3-common php7.3-mysql libapache2-mod-php7.3
 service apache2 restart
 
 # Install MySQL
-debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
-debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
+debconf-set-selections <<< "mysql-server mysql-server/root_password password $MYSQL_PASSWORD"
+debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $MYSQL_PASSWORD"
 apt-get install -y mysql-server
 
-# Download and Extract WordPress into the /var/www/html directory.
-cd /var/www/html && wget -c https://wordpress.org/latest.tar.gz -O - | tar -xz && cd wordpress && mv * ../ && cd ../ && rm -rf wordpress
-cd /var/www/html && touch .htaccess
+# Create MySQL database for WordPress.
+mysql -u$MYSQL_USERNAME -p$MYSQL_PASSWORD -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
+
+# Download and install WordPress CLI.
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+chmod +x wp-cli.phar
+mv wp-cli.phar /usr/local/bin/wp
+
+# Change directory to root directory of web server and
+# remove all files prior to installing WordPress.
+service apache2 stop
+cd /var/www
+rm -rf html
+mkdir html
+service apache2 start
+
+# Download WordPress, create wp-config, and create a user.
+cd /var/www/html
+wp core download --allow-root 
+wp config create --allow-root --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USERNAME --dbpass=$MYSQL_PASSWORD 
+wp core install --allow-root --url=$URL --title=$WORDPRESS_TITLE --admin_user=$WORDPRESS_USERNAME --admin_password=$WORDPRESS_PASSWORD --admin_email=$WORDPRESS_EMAIL
 
 # Change Apache config file to allow .htaccess rewrites.
 cp /vagrant/000-default.conf /etc/apache2/sites-available/000-default.conf
